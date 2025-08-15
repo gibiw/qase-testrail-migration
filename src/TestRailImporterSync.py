7ab21a008b44bdce39710fb5a70d37ec5f3d9df7
aortiz-qase <aortiz@qase.io>
2025-08-15 09:33:13 -0700

    undid the redundant pool code previously added

--- src/TestRailImporterSync.py
+++ src/TestRailImporterSync.py
@@ -1,19 +1,14 @@
-from .support import ConfigManager, Logger, Mappings, ThrottledThreadPoolExecutor, Pools
+from .support import ConfigManager, Logger, Mappings
 from .service import QaseService, TestrailService, QaseScimService
 from .entities import Users, Fields, Projects, Suites, Cases, Runs, Milestones, Configurations, Attachments, SharedSteps
-from concurrent.futures import ThreadPoolExecutor
+
 
 class TestRailImporterSync:
     def __init__(self, config: ConfigManager, logger: Logger) -> None:
-        self.pools = Pools(
-            qase_pool=ThrottledThreadPoolExecutor(max_workers=8, requests=230, interval=10),
-            tr_pool=ThreadPoolExecutor(max_workers=8),
-        )
-
         self.logger = logger
         self.config = config
         self.qase_scim_service = None
-        
+
         self.qase_service = QaseService(config, logger)
         if (config.get('qase.scim_token')):
             self.qase_scim_service = QaseScimService(config, logger)
@@ -27,95 +22,85 @@ class TestRailImporterSync:
     def start(self):
         # Step 1. Build users map
         self.mappings = Users(
-            self.qase_service, 
-            self.testrail_service, 
-            self.logger, 
+            self.qase_service,
+            self.testrail_service,
+            self.logger,
             self.mappings,
             self.config,
-            self.pools,
-            scim_service=self.qase_scim_service,
+            self.qase_scim_service,
         ).import_users()
 
         # Step 2. Import project and build projects map
         self.mappings = Projects(
-            self.qase_service, 
-            self.testrail_service, 
-            self.logger, 
+            self.qase_service,
+            self.testrail_service,
+            self.logger,
             self.mappings,
-            self.config,
-            self.pools,
+            self.config
         ).import_projects()
 
         # Step 3. Import attachments
         self.mappings = Attachments(
-            self.qase_service, 
-            self.testrail_service, 
-            self.logger, 
+            self.qase_service,
+            self.testrail_service,
+            self.logger,
             self.mappings,
-            self.config,
-            self.pools,
+            self.config
         ).import_all_attachments()
 
         # Step 4. Import custom fields
         self.mappings = Fields(
-            self.qase_service, 
-            self.testrail_service, 
-            self.logger, 
+            self.qase_service,
+            self.testrail_service,
+            self.logger,
             self.mappings,
             self.config,
-            self.pools,
         ).import_fields()
 
         for project in self.mappings.projects:
 
             self.mappings = Configurations(
-            self.qase_service, 
-            self.testrail_service, 
-            self.logger, 
-            self.mappings,
-            self.pools,
+                self.qase_service,
+                self.testrail_service,
+                self.logger,
+                self.mappings,
             ).import_configurations(project)
 
             self.mappings = SharedSteps(
-                self.qase_service, 
-                self.testrail_service, 
-                self.logger, 
+                self.qase_service,
+                self.testrail_service,
+                self.logger,
                 self.mappings,
-                self.pools,
             ).import_shared_steps(project)
 
             self.mappings = Milestones(
-                self.qase_service, 
-                self.testrail_service, 
-                self.logger, 
+                self.qase_service,
+                self.testrail_service,
+                self.logger,
                 self.mappings,
-                self.pools,
             ).import_milestones(project)
 
             self.mappings = Suites(
-                self.qase_service, 
-                self.testrail_service, 
-                self.logger, 
-                self.mappings, 
-                self.config,
-                self.pools,
+                self.qase_service,
+                self.testrail_service,
+                self.logger,
+                self.mappings,
+                self.config
             ).import_suites(project)
 
             Cases(
-                self.qase_service, 
-                self.testrail_service, 
-                self.logger, 
-                self.mappings, 
-                self.config,
-                self.pools,
+                self.qase_service,
+                self.testrail_service,
+                self.logger,
+                self.mappings,
+                self.config
             ).import_cases(project)
 
             Runs(
-                self.qase_service, 
-                self.testrail_service, 
-                self.logger, 
-                self.mappings, 
+                self.qase_service,
+                self.testrail_service,
+                self.logger,
+                self.mappings,
                 self.config,
-                project,
-                self.pools,
+                project
             ).import_runs()
